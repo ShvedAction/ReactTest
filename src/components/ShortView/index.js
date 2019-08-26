7876aa3e102e6af710a1cad63162f01236c8048b
Алексей Шведюк <shved@MacBook-Pro-Aleksej.local>
2019-08-26 07:21:53 +0300

    separate view by parts

--- src/components/ShortView/index.js
+++ src/components/ShortView/index.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux'
 import { toDetailsList } from '../../store/thunks'
 import { changeCurrentBy } from '../../store/short'
 import Button from '../Button'
-// import ListWrapper from '../ListWrapper'
-// import ViewFooter from '../ViewFooter'
+import {ViewWrapper, ItemsTable, ViewFooter} from '../view_parts'
 
 const mapStateToProps = ({ short }) => ({
   short
@@ -17,22 +16,22 @@ const mapDispatchToProps = {
 
 const ItemsMap = ({items, currentIndex}) =>
   items.map((el, ind) => (
-    <div  key={ind}>{el.name}</div>
+    <div className={"test"}  key={ind}>{el.name}</div>
   ))
 
 const ShortView = ({ short, toDetailsList, changeCurrentBy }) =>{ 
   const { currentIndex, canIncreaseCurrent, canDecreaseCurrent, canMoveItem, items } = short
   return (
-  <div>
-    <div>
+  <ViewWrapper>
+    <ItemsTable>
       <ItemsMap items={items} currentIndex={currentIndex}/>
-    </div>
-    <div>
+    </ItemsTable>
+    <ViewFooter>
       <Button activation={canIncreaseCurrent} click={()=> changeCurrentBy(1)}>D</Button>
       <Button activation={canDecreaseCurrent} click={()=> changeCurrentBy(-1)}>U</Button>
       <Button activation={canMoveItem} click={toDetailsList}>Добавить</Button>
-    </div>
-  </div>
+    </ViewFooter>
+  </ViewWrapper>
   )
 }
 
