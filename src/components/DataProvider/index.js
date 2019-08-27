639846cadb4e06f65cb52cd99f4e2820eb98fac4
Алексей Шведюк <shved@MacBook-Pro-Aleksej.local>
2019-08-27 07:00:07 +0300

    update ref to json

--- src/components/DataProvider/index.js
+++ src/components/DataProvider/index.js
@@ -2,13 +2,11 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { add as addProductToStore } from '../../store/short'
 
-const MAGIC_CONST = "https://gist.githubusercontent.com/ShvedAction/f855ce950310658c8d2c79ad20f186d1/raw/b6a30694cb8167df4c7f4990858b945097b411cd/fixture.json"
-
+const MAGIC_CONST = 'https://gist.githubusercontent.com/ShvedAction/f855ce950310658c8d2c79ad20f186d1/raw/4812af63ad261b240dc550075b425fac70bd437d/fixture.json'
 
 const mapDispatchToProps = {
   addProductToStore
 }
-
 
 const DataProvider = ({ addProductToStore, children }) => {
   useEffect(() => {
@@ -17,7 +15,8 @@ const DataProvider = ({ addProductToStore, children }) => {
     }).then(products => {
       console.log(products)
       return addProductToStore(products)
-    })})
+    })
+  })
   return (
     <>
       {children}
@@ -25,4 +24,4 @@ const DataProvider = ({ addProductToStore, children }) => {
   )
 }
 
-export default connect(()=> ({}), mapDispatchToProps)(DataProvider)
+export default connect(() => ({}), mapDispatchToProps)(DataProvider)
